06678ca729cef23a9bae58bc5345c94cedda5e99
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-06 02:48:48 -0500

    login page bug fixes code

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -3,6 +3,9 @@ public class LoginPage {
 public static void main(String[] args) {
    System.out.println("This is login  code");
    System.out.println("I hope thing will go on my way");
+   System.out.println("This is bug fixing branch");
+   System.out.println("This is bug fixing branch");
+   
 	}
 
 }
