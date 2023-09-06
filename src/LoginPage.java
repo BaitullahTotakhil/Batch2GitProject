4f89f08e9177147e038358830fe599ece558fb3b
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-06 02:03:55 -0500

    this as an other update

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,7 +1,8 @@
 
 public class LoginPage {
 public static void main(String[] args) {
-   System.out.println("This is login oage code");
+   System.out.println("This is login  code");
+   System.out.println("I hope thing will go on my way");
 	}
 
 }
