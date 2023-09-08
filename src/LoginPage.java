75816cb8712ce9a7e679647b02ecb46e248c4de3
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-08 03:57:07 -0500

    working

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -18,8 +18,7 @@ public static void main(String[] args) {
    System.out.println("an other line of code in Baitullahs page");
 >>>>>>> Stashed changes
 =======
-   System.out.println("Ya Allah bless teka me out of this defficulteis");
-   System.out.println("Ya Allah bless teka me out of this defficulteis");
+  
 
 >>>>>>> ad9b6c74136c65cac69baa078b90ddb73f77696e
 	}
