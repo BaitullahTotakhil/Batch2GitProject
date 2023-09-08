df6c739c62284b28eaf6e5e0df0007d7310e88d4
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-08 00:59:45 -0500

    just an other line of code

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -3,9 +3,12 @@ public class LoginPage {
 public static void main(String[] args) {
    System.out.println("This is login  code");
    System.out.println("I hope thing will go on my way");
+
    System.out.println("This is bug fixing branch");
    System.out.println("This is bug fixing branch");
-   
+   System.out.println("this is my second commint");
+   System.out.println("this is my second commint");
+   System.out.println("Just an other line of Code");
 	}
 
 }
