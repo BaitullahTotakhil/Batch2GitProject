a6b0295e05d1a41f12d4908371b9f8fc4189b07e
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-08 05:35:47 -0500

    signupPage Added

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -8,16 +8,7 @@ public static void main(String[] args) {
    System.out.println("This is bug fixing branch");
    System.out.println("This is bug fixing branch");
    System.out.println("this is my second commint");
-   System.out.println("this is my second commint");
-<<<<<<< Updated upstream
-   System.out.println("Just an other line of Code");
-   System.out.println("this is my second commint");
-   System.out.println("this is my second commint");
-   System.out.println("Just an other line of Code");
-=======
-   System.out.println("an other line of code in Baitullahs page");
->>>>>>> Stashed changes
-=======
+  
   
 
 >>>>>>> ad9b6c74136c65cac69baa078b90ddb73f77696e
