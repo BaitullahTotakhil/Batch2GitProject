892c701062478d790f25f7996dfa7974e221cdf8
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-08 01:06:29 -0500

    login page added

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -3,6 +3,7 @@ public class LoginPage {
 public static void main(String[] args) {
    System.out.println("This is login  code");
    System.out.println("I hope thing will go on my way");
+   System.out.println("Ya Allah bless teka me out of this defficulteis");
 	}
 
 }
