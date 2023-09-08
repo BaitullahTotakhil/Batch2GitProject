0919f423d8af363c844075ddc8c184e8c3888eef
BaitullahTotakhil <baitullahtotakhail2014@gmail.com>
2023-09-08 03:34:34 -0500

    working

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -8,10 +8,14 @@ public static void main(String[] args) {
    System.out.println("This is bug fixing branch");
    System.out.println("this is my second commint");
    System.out.println("this is my second commint");
+<<<<<<< Updated upstream
    System.out.println("Just an other line of Code");
    System.out.println("this is my second commint");
    System.out.println("this is my second commint");
    System.out.println("Just an other line of Code");
+=======
+   System.out.println("an other line of code in Baitullahs page");
+>>>>>>> Stashed changes
 	}
 
 }
